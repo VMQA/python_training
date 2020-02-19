9f59d244dea44b2d5783fc816001ef58a092ea0f
vmarochok <44023322+vmarochok@users.noreply.github.com>
2020-02-19 16:05:56 +0200

    Созданы вспомогательные методы

--- test_add_contact.py
+++ test_add_contact.py
@@ -11,32 +11,52 @@ class UntitledTestCase(unittest.TestCase):
     
     def test_untitled_test_case(self):
         driver = self.driver
-        driver.get("http://localhost:8080/addressbook/index.php")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_id("content").click()
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(driver)
+        self.login(driver)
+        self.create_contact(driver)
+        self.return_to_home_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self, driver):
+        driver.find_element_by_link_text("home page").click()
+
+    def create_contact(self, driver):
+        # init contact creation
         driver.find_element_by_link_text("add new").click()
+        # input First name
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
         driver.find_element_by_name("firstname").send_keys("John")
+        # input Last name
         driver.find_element_by_name("lastname").click()
         driver.find_element_by_name("lastname").clear()
         driver.find_element_by_name("lastname").send_keys("Smith")
+        # input address
         driver.find_element_by_name("address").click()
         driver.find_element_by_name("address").clear()
         driver.find_element_by_name("address").send_keys("186 Main str.")
+        # input email
         driver.find_element_by_name("email").click()
         driver.find_element_by_name("email").clear()
         driver.find_element_by_name("email").send_keys("test@test.com")
         driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        driver.find_element_by_link_text("home page").click()
-        driver.find_element_by_link_text("Logout").click()
-    
+
+    def login(self, driver):
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        # delete me if works - driver.find_element_by_id("content").click()
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://localhost:8080/addressbook/index.php")
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
