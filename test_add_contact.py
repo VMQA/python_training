97aef8e365f51f9e4ff4230a9a3b5b4383683285
vmarochok <44023322+vmarochok@users.noreply.github.com>
2020-02-21 15:40:52 +0200

    Home Page URL is changed

--- test_add_contact.py
+++ test_add_contact.py
@@ -9,11 +9,19 @@ class UntitledTestCase(unittest.TestCase):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
     
-    def test_untitled_test_case(self):
+    def test_add_contact(self):
         driver = self.driver
         self.open_home_page(driver)
-        self.login(driver)
-        self.create_contact(driver)
+        self.login(driver, username="admin", password="secret")
+        self.create_contact(driver, first_name="John", last_name="Smith", address="186 Main str.", email="test@test.com")
+        self.return_to_home_page(driver)
+        self.logout(driver)
+
+    def test_add_empty_contact(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.create_contact(driver, first_name="", last_name="", address="", email="")
         self.return_to_home_page(driver)
         self.logout(driver)
 
@@ -23,39 +31,39 @@ class UntitledTestCase(unittest.TestCase):
     def return_to_home_page(self, driver):
         driver.find_element_by_link_text("home page").click()
 
-    def create_contact(self, driver):
+    def create_contact(self, driver, first_name, last_name, address, email):
         # init contact creation
         driver.find_element_by_link_text("add new").click()
         # input First name
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("John")
+        driver.find_element_by_name("firstname").send_keys("%s" % first_name)
         # input Last name
         driver.find_element_by_name("lastname").click()
         driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("Smith")
+        driver.find_element_by_name("lastname").send_keys("%s" % last_name)
         # input address
         driver.find_element_by_name("address").click()
         driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("186 Main str.")
+        driver.find_element_by_name("address").send_keys("%s" % address)
         # input email
         driver.find_element_by_name("email").click()
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("test@test.com")
+        driver.find_element_by_name("email").send_keys("%s" % email)
         driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys("%s" % username)
         # delete me if works - driver.find_element_by_id("content").click()
         driver.find_element_by_name("pass").click()
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys("%s" % password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, driver):
-        driver.get("http://localhost:8080/addressbook/index.php")
+        driver.get("http://localhost/addressbook")
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
--- test_add_group.py
+++ test_add_group.py
@@ -63,7 +63,7 @@ class TestAddGroup(unittest.TestCase):
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, driver):
-        driver.get("http://localhost:8080/addressbook/index.php")
+        driver.get("http://localhost/addressbook")
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
